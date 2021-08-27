128267454f89128cb6737e09504f1c8dee2c54b7
wbt1208 <y8362114tuofan@163.com>
2021-08-27 09:43:48 +0800

    Update common.py

--- common/common.py
+++ common/common.py
@@ -41,24 +41,24 @@ class ConfUtil(ConfUtilFornatter):
     @staticmethod
     def set_default_conf():
         default_conf = {
-            "autosms_mode": 1,
-            "paramters": {
-                "source": "百家号",
-                "field": "全部",
-                "keyword": "",
-                "interval": "20",
-                "cookies": "UM_distinctid=17b694205d8261-0b9e0ad739305c-35617403-13c680-17b694205d934f; Identification=13353936525; PwdToken=44a072122c0c4650d9c39e7b8ca20660; mySpread=36HWDDZJ; Hm_lvt_119d728e13405b1761bac1057994ec52=1629558541,1629826666; Token=d6040574-6c8a-4bac-b1a9-4aabd2cabac3; WxScanOpenId=b2lDZTEwbldfWGtuaHZUM1pZOS14c212N0RqOA; ct=d6040574-6c8a-4bac-b1a9-4aabd2cabac3_1629895511448; CNZZDATA1278145158=2113046919-1629553933-null%7C1629895454; Hm_lpvt_119d728e13405b1761bac1057994ec52=1629895537; ckt=1629895550"
-            },
-            "html_path": ".\\html",
-            "word_path": ".\\wold",
-            "mysqldb": {
-                "host": "",
-                "port": "",
-                "user": "",
-                "passwd": "",
-                "dbname": ""
-            }
-        }
+    "autosms_mode": 1,
+    "paramters": {
+        "source": "百家号",
+        "field": "历史",
+        "keyword": ""
+           },
+    "interval": "20",
+    "cookies": "UM_distinctid=17b694205d8261-0b9e0ad739305c-35617403-13c680-17b694205d934f; Identification=13353936525; PwdToken=44a072122c0c4650d9c39e7b8ca20660; mySpread=36HWDDZJ; Hm_lvt_119d728e13405b1761bac1057994ec52=1629558541,1629826666; WxScanOpenId=b2lDZTEwbldfWGtuaHZUM1pZOS14c212N0RqOA; Token=9f5ca04b-00c6-4706-9079-1815f0c545f2; ct=9f5ca04b-00c6-4706-9079-1815f0c545f2_1629985876006; CNZZDATA1278145158=2113046919-1629553933-null%7C1629981519; ckt=1629985877; Hm_lpvt_119d728e13405b1761bac1057994ec52=1629985877",
+    "html_path": "html",
+    "word_path": "wold",
+    "mysqldb": {
+        "host": "",
+        "port": "",
+        "user": "",
+        "passwd": "",
+        "dbname": ""
+    }
+}
         with open("./conf.json", "w+", encoding="utf-8") as fp:
             json.dump(default_conf, fp, indent=4)
 
